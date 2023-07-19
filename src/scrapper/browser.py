1a112e12a518f019ebc088c5fe482db0f7d411db
Gustavo Santos <gustavo.santos@MAISVALOR.COM>
2023-07-19 15:39:40 -0300

    refactor: colocando webdriver para baixar automaticamente

--- src/scrapper/browser.py
+++ src/scrapper/browser.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
-
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 class Browser:
 
     def __init__(self) -> None:
-        self.chrome_options = webdriver.ChromeOptions()
+        self.chrome_options = Options()
         self.chrome_options.add_argument('--headless=new')
-        self.driver = webdriver.Chrome(options=self.chrome_options)
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.chrome_options)
 
     def close(self):
         if self:
